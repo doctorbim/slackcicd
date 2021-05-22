3490f80d416adf7d539b6ba21d18283db6e852ab
doctorbim <fosjun@gmail.com>
2021-05-22 17:48:51 -0400

    Changed test.js

--- Makefile
+++ Makefile
@@ -3,5 +3,5 @@ docker-dev:
 docker-prod:
 	docker build -t slackcicd:v0.1.0-prod -f build/Dockerfile.prod .
 run:
-	docker run -d -p 3000:3000 -e SLACK_SIGNING_SECRET=94a46143cfb81883e41778bc3a7d8831 -e SLACK_TOKEN=xoxb-324775556195-2099315281124-7p7ZhIHoG7WOiJXyT9ADbgqA slackcicd:v0.1.0-prod
+	docker run -d -p 3000:3000 -e SLACK_SIGNING_SECRET=94a46143cfb81883e41778bc3a7d8831 -e SLACK_TOKEN=xoxb-324775556195-2099315281124-BezHnZp7rqtyegqpsSdUs1Ub slackcicd:v0.1.0-prod
 	
--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const { WebClient } = require('@slack/web-api');
 const { createEventAdapter } = require('@slack/events-api');
 
-const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
-const slackToken = process.env.SLACK_TOKEN;
+const slackSigningSecret = '94a46143cfb81883e41778bc3a7d8831';
+const slackToken = process.env.'xoxb-324775556195-2099315281124-BezHnZp7rqtyegqpsSdUs1Ub';
 const port = process.env.SLACK_PORT || 3000;
 
 const slackEvents = createEventAdapter(slackSigningSecret);
