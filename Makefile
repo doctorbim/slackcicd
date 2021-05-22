ea7a12e8200d5cf636563757749dbce9762e6150
doctorbim <fosjun@gmail.com>
2021-05-22 14:54:22 -0400

    circleci

---
+++ Makefile
@@ -0,0 +1,7 @@
+docker-dev:
+	docker build -t slackcicd:v0.1.0-dev -f build/Dockerfile.dev .
+docker-prod:
+	docker build -t slackcicd:v0.1.0-prod -f build/Dockerfile.prod .
+run:
+	docker run -d -p 3000:3000 -e SLACK_SIGNING_SECRET=94a46143cfb81883e41778bc3a7d8831 -e SLACK_TOKEN=xoxb-324775556195-2099315281124-7p7ZhIHoG7WOiJXyT9ADbgqA slackcicd:v0.1.0-prod
+	
--- app.js
+++ app.js
@@ -9,18 +9,18 @@ const slackEvents = createEventAdapter(slackSigningSecret);
 const slackClient = new WebClient(slackToken);
 
 slackEvents.on('app_mention', (event) => {
-    console.log(`Got message from user ${event.user}: ${event.text}`);
-    (async () => {
-        try {
-            await slackClient.chat.postMessage({ channel: event.channel, text: `Hello <@${event.user}>! :tada:` });
-        } catch (error) {
-            console.log(error.data)
-        }
-    })();
+  console.log(`Got message from user ${event.user}: ${event.text}`);
+  (async () => {
+    try {
+      await slackClient.chat.postMessage({ channel: event.channel, text: `Hello <@${event.user}>! :tada:` })
+    } catch (error) {
+      console.log(error.data)
+    }
+  })();
 });
 
 slackEvents.on('error', console.error);
 
 slackEvents.start(port).then(() => {
-    console.log(`Server started on port ${port}`)
+  console.log(`Server started on port ${port}`)
 });
